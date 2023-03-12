c63d8523fe69d9bcf338ddb4d5c0cd479ed26bd2
Frank Eijking <frank@medianique.nl>
2023-03-12 11:42:30 +0100

    Add comments

--- src/3d/benchy/Model.tsx
+++ src/3d/benchy/Model.tsx
@@ -12,6 +12,10 @@ import model from './benchy.glb';
 export const Model: React.FC<MeshModel> = ({ color = modelColor, ...props }) => {
 	const { rotate } = useWrapper();
 	const { scene, materials } = useGLTF(model) as unknown as GLTFResult;
+	/**
+	 * To use a material by name as below you need to define or look this up in the glTF file you're importing.
+	 * You could do so via e.g. Blender. See: https://medianique.nl/blog/virtual-3d-printing
+	 */
 	useMemo(() => {
 		applyProps(materials.compound, {
 			color,
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 import { Benchy3d } from './3d/benchy/Benchy3d';
+/**
+ * The below Benchy3d component is a minimal scene used as described in the Virtual 3D Printing
+ * blog post: https://medianique.nl/blog/virtual-3d-printing
+ */
 // import { Benchy3d } from './3d/benchy/minimal/Benchy3d';
 import { backgroundColor } from './theme/colors';
 
